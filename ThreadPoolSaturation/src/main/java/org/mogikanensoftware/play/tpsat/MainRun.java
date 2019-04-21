641416c601c48471d1a61947731c8a1b0d0b4f34
Vladislav Voinov <vladislav.voinov@ontariomd.com>
2019-04-21 17:12:29 -0400

    minor refactoring

--- ThreadPoolSaturation/src/main/java/org/mogikanensoftware/play/tpsat/MainRun.java
+++ ThreadPoolSaturation/src/main/java/org/mogikanensoftware/play/tpsat/MainRun.java
@@ -1,8 +1,6 @@
 package org.mogikanensoftware.play.tpsat;
 
-import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
@@ -12,31 +10,41 @@ public class MainRun {
     private static final int N_THREADS = 2;
 
     public static void main(String[] args) {
-        
-        //ThreadPoolExecutor exec = (ThreadPoolExecutor) Executors.newFixedThreadPool(2);
 
-        ThreadPoolExecutor exec
-                = new ThreadPoolExecutor(N_THREADS, N_THREADS,
-                        0L, TimeUnit.MILLISECONDS,
-                        new LinkedBlockingQueue<Runnable>(CAPACITY));
-    
+        ThreadPoolExecutor exec = new ThreadPoolExecutor(N_THREADS, N_THREADS, 0L, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<Runnable>(CAPACITY));
+
         exec.setRejectedExecutionHandler(new ThreadPoolExecutor.AbortPolicy());
-       
+
         for (int i = 0; i < 5; i++) {
-            final int count = i;
-            exec.execute(() -> {
-                String tName = Thread.currentThread().getName();
-                System.out.println("Doing some work in thread->"+tName+", count->"+count);
-                try {
-                    Thread.sleep(5000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                    throw new RuntimeException(e);
-                }
-                System.out.println("Work is done in thread->"+tName+", count->"+count);
-            }); 
-        } 
-       
+            exec.execute(new MyWorkThread(i));
+        }
+
         exec.shutdown();
     }
+}
+
+class MyWorkThread implements Runnable {
+
+    private int count;
+
+    public MyWorkThread(int count){
+        this.count = count;
+    }
+
+    @Override
+    public void run() {
+        String tName = Thread.currentThread().getName();
+        System.out.println("Doing some work in thread->" + tName + ", count->" + count);
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        System.out.println("Work is done in thread->" + tName + ", count->" + count);
+    }
+
+    
+
 }
